1b60fd5be07f4c2bb0451925505fc384158aac8c
lqm <1528490339@qq.com>
2018-08-19 16:07:31 +0800

    A、A2更新

--- src/A.java
+++ src/A.java
@@ -2,5 +2,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("11111111111");
         System.out.println("第二行");
+        System.out.println("第三行，时间1");
     }
 }
--- src/A2.java
+++ src/A2.java
@@ -1,2 +1,5 @@
 public class A2 {
+    public static void main(String[] args) {
+        System.out.println("时间1");
+    }
 }
