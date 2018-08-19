3aef24ead37c3231f280bd257f84b03586248f46
lqm1528490339 <1528490339@qq.com>
2018-08-19 17:44:12 +0800

    Update Lqm.java

--- src/cn/pp95/Lqm.java
+++ src/cn/pp95/Lqm.java
@@ -3,5 +3,6 @@ package cn.pp95;
 public class Lqm {
     public static void main(String[] args) {
         System.out.println("廖权名1111111111111");
+        System.out.println("廖权名1111111111111");
     }
 }
